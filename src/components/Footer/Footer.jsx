5f8d3997d9c8413f1e3eb43a073d9eaad5669f5f
elen-purple <anna.havrylova.2011@gmail.com>
2025-07-23 14:18:33 +0300

    makes footer styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,51 +3,59 @@ import discord from "../../icons/discord.svg";
 import x from "../../icons/x.svg";
 import meta from "../../icons/meta.svg";
 import { Button } from "../Button/Button";
+import styles from "./Footer.module.scss";
+import { Container } from "../Container/Container";
 export const Footer = () => {
   return (
-    <footer>
-      <h2>Sound. Presence. Purpose.</h2>
-      <p>
-        Focused on crafting immersive audio experiences.
-        <br />
-        We believe sound should not just be heard—it should move you.
-      </p>
-      <div>
-        <Button text="Aura Pro II" />
-        <Button text="Flow II" />
-      </div>
-      <hr />
-      <div>
-        <a href="#">
-          <svg width="139" height="26">
-            <use href={logo}></use>
-          </svg>
-        </a>
-        <p>© 2025 Resonance. All rights reserved.</p>
-        <ul>
-          <li>
-            <a href="#">
-              <svg width="26" height="26">
-                <use href={discord}></use>
-              </svg>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <svg width="26" height="26">
-                <use href={x}></use>
-              </svg>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <svg width="26" height="26">
-                <use href={meta}></use>
-              </svg>
-            </a>
-          </li>
-        </ul>
-      </div>
+    <footer className={styles.footer}>
+      <Container>
+        <div className={styles.footer__header}>
+          <h2 className={styles.footer__title}>Sound. Presence. Purpose.</h2>
+          <p className={styles.footer__desc}>
+            Focused on crafting immersive audio experiences.
+            <br />
+            We believe sound should not just be heard—it should move you.
+          </p>
+          <div className={styles.footer__btns}>
+            <Button text="Aura Pro II" />
+            <Button text="Flow II" />
+          </div>
+        </div>
+        <div className={styles.footer__line}></div>
+        <div className={styles.footer__bottom}>
+          <a className={styles.footer__logo} href="#">
+            <svg className={styles.footer__icon} width="139" height="26">
+              <use href={logo}></use>
+            </svg>
+          </a>
+          <p className={styles.footer__copy}>
+            © 2025 Resonance. All rights reserved.
+          </p>
+          <ul className={styles.footer__list}>
+            <li className={styles.footer__item}>
+              <a className={styles.footer__link} href="#">
+                <svg className={styles.footer__media} width="26" height="26">
+                  <use href={discord}></use>
+                </svg>
+              </a>
+            </li>
+            <li className={styles.footer__item}>
+              <a className={styles.footer__link} href="#">
+                <svg className={styles.footer__media} width="26" height="26">
+                  <use href={x}></use>
+                </svg>
+              </a>
+            </li>
+            <li className={styles.footer__item}>
+              <a className={styles.footer__link} href="#">
+                <svg className={styles.footer__media} width="26" height="26">
+                  <use href={meta}></use>
+                </svg>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </Container>
     </footer>
   );
 };
