262c3cfe055af06f6db5e80a5a4f7775faab3594
elen-purple <anna.havrylova.2011@gmail.com>
2025-07-23 09:49:00 +0300

    makes header scss

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,8 @@
+import styles from "./Button.module.scss";
 export const Button = ({ text, isDark = false }) => {
   return (
-    <a data-dark={isDark} href="#">
-      {text}
+    <a className={styles.btn} data-dark={isDark} href="#">
+      <span className={styles.btn__text}>{text}</span>
     </a>
   );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,36 @@
+import burger from "../../icons/burger.svg";
 import logo from "../../icons/logo.svg";
 import { Button } from "../Button/Button";
+import { Container } from "../Container/Container";
+import styles from "./Header.module.scss";
 export const Header = ({}) => {
   return (
-    <header>
-      <a href="#">
-        <svg width="139" height="26">
-          <use href={logo}></use>
-        </svg>
-      </a>
-      <ul>
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#">Tech Specs</a>
-        </li>
-      </ul>
-      <Button text="Compare" isDark={true} />
+    <header className={styles.header}>
+      <Container>
+        <a className={styles.header__logo} href="#">
+          <svg className={styles.header__icon} width="139" height="26">
+            <use href={logo}></use>
+          </svg>
+        </a>
+        <ul className={styles.header__list}>
+          <li className={styles.header__item}>
+            <a className={styles.header__link} href="#">
+              Home
+            </a>
+          </li>
+          <li className={styles.header__item}>
+            <a className={styles.header__link} href="#">
+              Tech Specs
+            </a>
+          </li>
+        </ul>
+        <Button text="Compare" isDark={true} />
+        <button className={styles.header__menu} type="button">
+          <svg className={styles.header__burger} width="32" height="32">
+            <use href={burger}></use>
+          </svg>
+        </button>
+      </Container>
     </header>
   );
 };
