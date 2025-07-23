8d26c4ba475e104467447a23b1bf72ba106bfec0
elen-purple <anna.havrylova.2011@gmail.com>
2025-07-23 10:11:21 +0300

    makes hero

--- src/pages/HomePage/Hero/Hero.jsx
+++ src/pages/HomePage/Hero/Hero.jsx
@@ -1,15 +1,26 @@
 import imgmobile from "../../../imgs/mobile/homepage/hero/headphone@mobile.webp";
 import imgdesktop from "../../../imgs/desktop/homepage/hero/headphone@desktop.webp";
+import styles from "./Hero.module.scss";
+import { Container } from "../../../components/Container/Container";
 export const Hero = () => {
   return (
-    <section>
-      <h1>Resonance</h1>
-      <h2>Aura Pro II</h2>
-      <picture>
-        <source srcSet={imgdesktop} media="(min-width: 1120px)" />
-        <source srcSet={imgmobile} media="(max-width: 1119px)" />
-        <img width="420" height="420" src={imgmobile} alt="Headphones" />
-      </picture>
+    <section className={styles.hero}>
+      <Container>
+        <h1 className={styles.hero__title}>Resonance</h1>
+        <h2 className={styles.hero__subtitle}>Aura Pro II</h2>
+        <picture>
+          <source srcSet={imgdesktop} media="(min-width: 1120px)" />
+          <source srcSet={imgmobile} media="(max-width: 1119px)" />
+          <img
+            className={styles.hero__img}
+            width="420"
+            height="420"
+            src={imgmobile}
+            alt="Headphones"
+          />
+        </picture>
+        <div className={styles.hero__bg}></div>
+      </Container>
     </section>
   );
 };
